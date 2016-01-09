c45644a5d5110ec4f49044ea30a1829bd44e1681
3ventic <3ventic@gmail.com>
2016-01-09 07:46:12 +0200

    first working version

--- ShortUrl/server.js
+++ ShortUrl/server.js
@@ -2,144 +2,226 @@
 var url = require('url');
 var qs = require('querystring');
 var sqlite3 = require('sqlite3').verbose();
-var config = require('./config.sample.js');
+var config = require('./config.js');
 var port = config.port || 8000;
 
-var db = new sqlite3.Database('links.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, function (err) {
-    if (err) {
+console.log("Starting app");
+
+var db = new sqlite3.Database('links.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, function (err)
+{
+    if (err)
+    {
         console.log(err);
         process.exit(1);
     }
+    console.log("Starting DB");
     
-    db.serialize(function () {
-        db.run("CREATE TABLE IF NOT EXISTS links (path TEXT PRIMARY KEY NOT NULL, destination TEXT NOT NULL)");
+    db.serialize(function ()
+    {
+        db.run("CREATE TABLE IF NOT EXISTS links (path TEXT PRIMARY KEY NOT NULL, destination TEXT NOT NULL, used INT NOT NULL)", function (err)
+        {
+            if (err)
+            {
+                console.log(err);
+                process.exit(2);
+            }
+            console.log("Started DB");
+        });
     });
 });
 
-http.createServer(function (req, res) {
-    var postData = '';
-    
-    if (req.method == "POST") {
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-            if (body.length > 1e3)
-                req.connection.destroy();
-        });
-        req.constructor('end', function () {
-            postData = qs.parse(body);
-        });
-    }
-    
+http.createServer(function (req, res)
+{
     var url_params = url.parse(req.url);
-    if (url_params.pathname.length > 2 && url_params.pathname[2] == '.') {
+    var query = qs.parse(url_params.query);
+    console.log(url_params);
+    console.log(query);
+    if (url_params.pathname && url_params.pathname.length > 2 && url_params.pathname[2] == '.')
+    {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end("Not found");
     }
-    else if (url_params.query == "secret=" + config.secret) {
-        switch (url_params.pathname) {
+    else if (query.secret == config.secret)
+    {
+        switch (url_params.pathname)
+        {
             case "":
             case "/":
                 res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(getFrontPageHtml());
+                getFrontPageHtml(function (html)
+                {
+                    res.end(html);
+                });
                 break;
             case "/new":
-                if (post["target"].length == 0) {
+                if (typeof query.target === "undefined" || query.target.length == 0)
+                {
                     returnError(res, 400, "Bad Request: Target must be specified");
                 }
-                else {
+                else
+                {
                     var path,
-                        target = post["target"];
-                    if (post["path"].length > 0) {
-                        path = post["path"];
+                        target = query.target;
+                    if (typeof query.path !== "undefined" && query.path.length > 0)
+                    {
+                        path = query.path;
                         newLink(res, path, target);
                     }
-                    else {
-                        db.get('SELECT Count(*) AS count FROM links', function (err, row) {
-                            if (err) {
+                    else
+                    {
+                        db.get('SELECT MAX(rowid) FROM links', function (err, row)
+                        {
+                            if (err)
+                            {
                                 returnError(res, 500, "SQL Error");
                             }
-                            else {
-                                newLink(res, numToPath(row["count"] + 1), target);
+                            else
+                            {
+                                newLink(res, numToPath((row.rowid || -1) + 1), target);
                             }
                         });
                     }
                 }
                 break;
             default:
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end("Not found");
+                returnError(res, 404, "Not Found");
                 break;
         }
     }
-    else {
+    else if (url_params.pathname.length < 2)
+    {
+        returnError(res, 403, "Forbidden to access /");
+    }
+    else
+    {
+        var path = url_params.pathname.substring(1);
         // Follow redirect
+        db.get('SELECT destination FROM links WHERE path LIKE $path', {
+            $path: path
+        }, function (err, row)
+        {
+            if (err)
+            {
+                console.log(err);
+                returnError(res, 500, "SQL Error");
+            }
+            else
+            {
+                res.writeHead(301, { Location: row.destination });
+                res.end();
+                db.run('UPDATE links SET used = used + 1 WHERE $path', {
+                    $path: path
+                });
+            }
+        });
     }
 }).listen(port);
+console.log("Starting HTTP");
 
 var validChars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890_-';
 
-function numToPath(num) {
+function numToPath(num)
+{
     if (num < validChars.length)
         return validChars[num];
     return numToPath(num / validChars.length) + numToPath(num % validChars.length);
 }
 
-function returnError(res, code, message) {
+function returnError(res, code, message)
+{
     res.writeHead(code, { 'Content-Type': 'application/json' });
-    res.end({
+    res.end(JSON.stringify({
         status: code,
         msg: message
-    });
+    }));
 }
 
-function newLink(res, path, target) {
-    db.run('INSERT OR IGNORE INTO links VALUES ($path, $destination)', {
+function newLink(res, path, target)
+{
+    db.run('INSERT OR IGNORE INTO links VALUES ($path, $destination, 0)', {
         $path: path,
         $destination: target
-    }, function (err) {
-        if (err) {
+    }, function (err)
+    {
+        if (err)
+        {
             console.log(err);
             returnError(res, 500, "SQL Error");
         }
-        else {
+        else
+        {
             db.run('UPDATE links SET destination = $destination WHERE path LIKE $path', {
                 $path: path,
                 $destination: target
-            }, function (err) {
-                if (err) {
+            }, function (err)
+            {
+                if (err)
+                {
                     console.log(err);
                     returnError(res, 500, "SQL Error");
                 }
-                else {
+                else
+                {
                     res.writeHead(200, { 'Content-Type': 'application/json' });
-                    res.end({
+                    res.end(JSON.stringify({
                         status: 200,
-                        short: config.base_url + 'test',
-                        destination: ''
-                    });
+                        short: config.base_url + path,
+                        destination: target
+                    }));
                 }
             });
         }
     });
 }
 
-function escapeHTML(html) {
-    var text = html.replace(/["&'\/<>]/g, function (idx) {
-        return ['&quot;', '&amp;', '&#39;', '&#47;', '&lt;', '&gt;'][idx];
+function HtmlReplacer(c) {
+    return { '"': '&quot;', '&': '&amp;', "'": '&#39;', '/': '&#47;', '<': '&lt;', '>': '&gt;' }[c] || "[UNKNOWN]";
+}
+
+function escapeHtml(text)
+{
+    return text.toString().replace(/["&'\/<>]/g, HtmlReplacer);
+}
+
+function getFrontPageHtml(callback)
+{
+    var html = '<!DOCTYPE html><html><head><meta charset="utf-8" /><title>Shorten Link</title></head><body>' +
+        '<form  action="/new" method="GET"><input type="hidden" name="secret" value="' + config.secret +
+        '" /><input type="text" name="path" placeholder="(optional) path" /><input type="url" name="target" placeholder="target" /><input name="submit" type="submit" value="Create" /></form><ul>';
+    
+    db.each("SELECT rowid AS id, path, destination, used FROM links", function (err, row)
+    {
+        if (err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log(row.id + " (" + row.used + ") " + row.path + ": " + row.destination);
+            html += '<li>' + row.id + " (" + row.used + ") " + escapeHtml(row.path) + ": " + escapeHtml(row.destination) + '</li>';
+        }
+    }, function (err) {
+        if (err)
+        {
+            console.log(err);
+        }
+        html += '</ul></body></html>';
+        callback(html);
     });
 }
 
-function getFrontPageHtml() {
-    var html = '<!DOCTYPE html><html><head><meta charset="utf-8" /><title>Shorten Link</title></head><body>' +
-        '<form action="/new?secret=' + config.secret +
-        '" method=POST><input type=text name=path placeholder="(optional) path" /><input type=url name=target placeholder=target /><input type=submit value=Create /></form><ul>';
-    
-    db.each("SELECT rowid AS id, path, destination FROM links", function (err, row) {
-        console.log(row.id + " " + row.path + ": " + row.destination);
-        html += '<li>' + escapeHTML(row.id) + " " + escapeHTML(row.path) + ": " + escapeHTML(row.destination) + '</li>';
+function exitHandler() {
+    console.log("Closing db connection");
+    db.close(function (err)
+    {
+        if (err)
+        {
+            console.log(err);
+            process.exit(3);
+        }
+        else
+            process.exit(0);
     });
-    html += '</ul></body></html>';
-    
-    return html;
 }
+
+process.on('SIGINT', exitHandler);
